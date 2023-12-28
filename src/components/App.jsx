11de9222778e2c54a385f0722e8291103d4a5e4f
VickyUA <vickyy379@gmail.com>
2023-12-28 23:43:48 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,20 @@ export default class App extends Component {
     filter: '',
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
   formSubmitHandler = ({ name, number }) => {
     const newContact = { name, number, id: nanoid() };
 
